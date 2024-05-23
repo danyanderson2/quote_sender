c539dee25a0d48eeaa50c74ab10aa154c414431a
danyanderson2 <danyanderson2222@gmail.com>
2024-05-24 00:54:57 +0100

    make last adjustments before final push

--- main.py
+++ main.py
@@ -12,33 +12,43 @@ import os
 import re
 import unicodedata
 
-TWITTER_PASSWORD = "@2Gether"
+# NO NEED TO GO BENEATH THIS BOUNDARIES IF YOU'RE A NON DEV :)
+##################################################################################################################
 
-def unicode_escape(text):
-    # Replace non-BMP characters with their Unicode escape sequences
-    return re.sub(r'[^\u0000-\uFFFF]', lambda x: '\\U{:08X}'.format(ord(x.group())), text)
-# def normalize_unicode(text):
-#     return unicodedata.normalize('NFKD', text)
-
-
-
+TWITTER_USERNAME = "" # your twitter username here
+TWITTER_PASSWORD = "" # your twitter password here
+GMAIL_APP_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD') # your gmail password app here
 TWITTER_EMAIL = "dany.guimefack@centrale-casablanca.ma"
+CHROMEDRIVER_PATH = '' # path to your chrome driver. Your browser must be google chrome
+EMAIL_SENDER='' # your email adress
+prog = 0 # saves value of last sent email to prevent an email to be sent twice
+
+####################################################################################################################
+
+
+# Function to replace non-BMP characters with their Unicode escape sequences
+def unicode_escape(text):
+    return re.sub(r'[^\u0000-\uFFFF]', lambda x: '\\U{:08X}'.format(ord(x.group())), text)
+
+
+# Setting driver options for chrome
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True )
-email_sender='danyanderson2222@gmail.com'
-prog = 0
+
+
+# Investigating latest progress through the list of quotes
 with open('progress.txt', 'r') as progress:
     a = progress.read().strip()
     score = int(a)
 # print(score)
+
+# Getting hold of the quotes, author and emojis
 for i in range(score,score+1):
     author = quotes[i]['author']
     emoji= quotes[i]['emoji']
     emoji_tweet = '✨'
     body = "\"" + quotes[i]['body']+ "\""
     emoji_tweet = unicode_escape(emoji_tweet)
-
-    # print(author,"\n",emoji,"\n",containt)
 
     with open('finished.txt', 'w',encoding='utf-8') as file:
         with open('template.txt', 'r') as temp:
@@ -47,26 +57,25 @@ for i in range(score,score+1):
         file.write(quote_replace)
 
 
-# Send mail
+# getting hold of final content to be sent via mail
 with open('finished.txt', 'r', encoding='utf-8') as finished:
     content = finished.read()
+
+
+# send an email to yourself
 msg = MIMEText(content, 'plain', 'utf-8')
-msg['Subject'] = "Motivational"
-
-
-# content = unicode_escape(content)
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=email_sender, password=os.environ.get('GMAIL_APP_PASSWORD'))
-#     connection.sendmail(from_addr=email_sender,
-#                         to_addrs='dany.guimefack@centrale-casablanca.ma',
-#                         msg=msg.as_string(),
-#
-#                        )
+msg['Subject'] = "Motivational Quote"
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=EMAIL_SENDER, password=GMAIL_APP_PASSWORD)
+    connection.sendmail(from_addr=EMAIL_SENDER,
+                        to_addrs=EMAIL_SENDER,
+                        msg=msg.as_string(),
+                        )
 
 
 # make a tweet
-driver = webdriver.Chrome(executable_path=r'D:\Python Extra\webdrivers\chrome\version 125.0.6422\chromedriver.exe',options=chrome_options)
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,options=chrome_options)
 driver.get("https://twitter.com/i/flow/login")
 time.sleep(5)
 email=driver.find_element(By.NAME,value="text")
@@ -80,7 +89,7 @@ try:
 except NoSuchElementException:
     print("Unhandled Exception")
 else:
-    user_name.send_keys("@IAmDanyAnderson")
+    user_name.send_keys(TWITTER_USERNAME)
     user_name.send_keys(Keys.ENTER)
     time.sleep(3)
 password = driver.find_element(By.NAME,value="password")
@@ -88,22 +97,16 @@ password.send_keys(TWITTER_PASSWORD)
 password.send_keys(Keys.ENTER)
 time.sleep(1)
 print(content)
-time.sleep(5)
-# post_button = driver.find_element(By.XPATH,value='//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a')
-# time.sleep(2)
-# post_button.click()
 time.sleep(10)
 my_post = driver.find_element(By.XPATH,value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
 time.sleep(3)
 my_post.send_keys(f"{body}\n\n {author} {emoji_tweet}")
-# script = f"arguments[0].value = '{content}';"
-# driver.execute_script(script, my_post)
 time.sleep(3)
 post_button = driver.find_element(By.XPATH,value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/button/div/span/span')
 print('clicked')
-# post_button.click()
-# time.sleep(5)
-# driver.quit()
+post_button.click()
+time.sleep(7)
+driver.quit()
 
 
 # make sure the quote is different everyday
